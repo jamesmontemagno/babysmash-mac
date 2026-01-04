861ea34242bc1f7e2a8f9cfa17c4c56021d3b967
James <james.montemagno@gmail.com>
2026-01-04 15:06:03 -0800

    Initial Commit

---
+++ babysmash/babysmashApp.swift
@@ -0,0 +1,17 @@
+//
+//  babysmashApp.swift
+//  babysmash
+//
+//  Created by James Montemagno on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct babysmashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
